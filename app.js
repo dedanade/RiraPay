adb9fe1a9dfda6d97a381b9cfccd1ba24bee32bd
Daniel Adebonojo <dedan2343@gmail.com>
2020-06-18 06:07:40 +0100

    new update

--- app.js
+++ app.js
@@ -44,7 +44,11 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.post('/mywebhook', Ordercontroler.paystackwebhook);
+app.post(
+  '/mywebhook',
+  express.raw({ type: 'application/json' }),
+  Ordercontroler.paystackwebhook
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
